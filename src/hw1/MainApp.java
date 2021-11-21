5fa043391b69797326bf848c8764dd797b1111df
NikitaLubimov <ikarus11122@gmail.com>
2021-11-21 10:10:42 +0300

    hw1

--- src/hw1/MainApp.java
+++ src/hw1/MainApp.java
@@ -2,6 +2,42 @@ package hw1;
 
 public class MainApp {
     public static void main(String[] args) {
+        printThreeWords();
+        checkSumSing(2, 3);
+        printColor(120);
+        compareNumbers(5, 3);
+    }
 
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSing(int a, int b) {
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor(int value) {
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(int a, int b) {
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 }
