3ceb22c7504e1fa1c34e9a69cf226f3b92e75ec4
NikitaLubimov <ikarus11122@gmail.com>
2021-11-21 20:20:19 +0300

    hw2

--- src/hw1/MainApp.java
+++ src/hw1/MainApp.java
@@ -2,42 +2,45 @@ package hw1;
 
 public class MainApp {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSing(2, 3);
-        printColor(120);
-        compareNumbers(5, 3);
+        System.out.println(test1(5, 6));
+        test2(-5);
+        System.out.println(test3(5));
+        test4("Привет", 2);
+        System.out.println(test5(1996));
     }
 
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-
-    public static void checkSumSing(int a, int b) {
+    public static boolean test1(int a, int b) {
         int c = a + b;
-        if (c >= 0) {
-            System.out.println("Сумма положительная");
+        return c >= 10 && c <= 20;
+    }
+
+    public static void test2(int a) {
+        if (a < 0) {
+            System.out.println("Переданое число отрицательное");
         } else {
-            System.out.println("Сумма отрицательная");
+            System.out.println("Переданое число положительное");
         }
     }
 
-    public static void printColor(int value) {
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
-        } else {
-            System.out.println("Зеленый");
+    public static boolean test3(int a) {
+        return a <= 0;
+    }
+
+    public static void test4(String a, int b) {
+        for (int i = 0; i < b; i++) {
+            System.out.println(a);
         }
     }
 
-    public static void compareNumbers(int a, int b) {
-        if (a >= b) {
-            System.out.println("a >= b");
+    public static boolean test5(int a) {
+        if (a % 4 != 0) {
+            return false;
+        } else if (a % 100 != 0) {
+            return true;
+        } else if (a % 400 == 0) {
+            return true;
         } else {
-            System.out.println("a < b");
+            return false;
         }
     }
 }
