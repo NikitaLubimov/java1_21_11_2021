349b510e58e5e539ee06f50dac25a5c939ee11ae
NikitaLubimov <ikarus11122@gmail.com>
2021-12-01 21:41:06 +0300

    hw4

--- src/hw1/MainApp.java
+++ src/hw1/MainApp.java
@@ -1,88 +1,136 @@
 package hw1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class MainApp {
+    static final int SIZE = 5;
+    static final int DOTS_TO_WIN = 3;
+
+    static final char DOT_X = 'X';
+    static final char DOT_O = 'O';
+    static final char DOT_EMPTY = '.';
+
+    static char[][] map;
+
+    static Scanner sc = new Scanner(System.in);
+    static Random random = new Random();
+
     public static void main(String[] args) {
-        int[] arr6 = new int[]{6, 5, 2, 1, 4, 400, 0, -1, -20, 100, -200};
-        print(arr5(5, 2));
-        System.out.println(getMin(arr6));
-        System.out.println(getMax(arr6));
-        int[] arr = new int[]{0, 0, 0, 0, 1, 1, 1, 1};    // 1 задача
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                arr[i] = 1;
-            } else {
-                arr[i] = 0;
+        initMap();
+        printMap();
+
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWinLines(DOT_X, DOTS_TO_WIN)) {
+                System.out.println("Поздравляем! Вы выиграли!");
+                break;
             }
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-
-        int[] arr2 = new int[100];                         // 2 задача
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = i;
-            System.out.print(arr2[i] + " ");
-        }
-        System.out.println();
-
-        int[] arr3 = new int[]{1, 2, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};       // 3 задача
-        for (int i = 0; i < arr3.length; i++) {
-            if (arr3[i] < 6) {
-                arr3[i] *= 2;
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
             }
-            System.out.print(arr3[i] + " ");
+
+            aiTurn();
+            printMap();
+            if (checkWinLines(DOT_O, DOTS_TO_WIN)) {
+                System.out.println("Копьютер победил.");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        System.out.print("  ");
+        for (int i = 1; i <= SIZE; i++) {
+            System.out.print(i + " ");
         }
         System.out.println();
-
-        int[][] arr4 = new int[5][5];                             // 4 задача
-        for (int i = 0; i < arr4.length; i++) {
-            for (int j = 0; j < arr4[i].length; j++) {
-                if (i == j) {
-                    arr4[i][j] = 1;
-                }
-                if (i + j == arr4.length - 1) {
-                    arr4[i][j] = 1;
-                }
-                System.out.printf("%4d", arr4[i][j]);
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.printf("%c ", map[i][j]);
             }
             System.out.println();
         }
     }
 
-    public static int[] arr5(int len, int initialValue) {         // 5 задача
-        int[] arr5 = new int[len];
-        for (int i = 0; i < arr5.length; i++) {
-            arr5[i] = initialValue;
-        }
-        return arr5;
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("input X,Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(y, x));
+        map[y][x] = DOT_X;
     }
 
-    public static void print(int[] arr) {                   // для себя метод что бы печатать масив
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+    public static boolean isCellValid(int y, int x) {
+        if (y < 0 || x < 0 || y >= SIZE || x >= SIZE) {
+            return false;
         }
-        System.out.println();
+        return map[y][x] == DOT_EMPTY;
     }
 
-    public static int getMin(int[] array) {                 // 6 задача. 1 метод для нахождения минимального элемента
-        int a = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < a) {
-                a = array[i];
+    public static void aiTurn() {
+        int x, y;
+        do {
+            y = random.nextInt(SIZE);
+            x = random.nextInt(SIZE);
+        } while (!isCellValid(y, x));
+        map[y][x] = DOT_O;
+    }
+
+    public static boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
             }
         }
-        return a;
+        return true;
     }
 
-    public static int getMax(int[] array) {                 // 6 задача. 2 метод для нахождения максимального элемента
-        int a = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > a) {
-                a = array[i];
+    static boolean checkLine(int cy, int cx, int vy, int vx, char dot, int dotsToWin) {
+        if (cx + vx + (dotsToWin - 1) > SIZE - 1 || cy + vy * (dotsToWin - 1) > SIZE - 1 || cy + vy * (dotsToWin - 1) < 0) {
+            return false;
+        }
+
+        for (int i = 0; i < dotsToWin; i++) {
+            if (map[cy + i * vy][cx + i * vx] != dot) {
+                return false;
             }
         }
-        return a;
+        return true;
+    }
+
+    public static boolean checkWinLines(char dot, int dotsToWin) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (checkLine(i, j, 0, 1, dot, dotsToWin) ||
+                        checkLine(i, j, 1, 0, dot, dotsToWin) ||
+                        checkLine(i, j, 1, 1, dot, dotsToWin) ||
+                        checkLine(i, j, -1, 1, dot, dotsToWin)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
-/*
-* На 7 и 8 задачу не хватило времени. Для себя в будущем обязательно сделаю.
-* */
+
