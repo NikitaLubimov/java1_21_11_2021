ccee44a87c32b5d2897d2444cb22e15578abb57e
NikitaLubimov <ikarus11122@gmail.com>
2021-12-02 08:02:40 +0300

    hw5

--- src/hw1/MainApp.java
+++ src/hw1/MainApp.java
@@ -1,136 +1,20 @@
 package hw1;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class MainApp {
-    static final int SIZE = 5;
-    static final int DOTS_TO_WIN = 3;
-
-    static final char DOT_X = 'X';
-    static final char DOT_O = 'O';
-    static final char DOT_EMPTY = '.';
-
-    static char[][] map;
-
-    static Scanner sc = new Scanner(System.in);
-    static Random random = new Random();
 
     public static void main(String[] args) {
-        initMap();
-        printMap();
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("nikita", "programmer", "Nikita@mail.ru", 79784, 15000, 25);
+        employees[1] = new Employee("sasha", "teacher", "sasha@mail.ru", 12345, 22000, 30);
+        employees[2] = new Employee("will", "driver", "will@mail.ru", 56768, 12000, 42);
+        employees[3] = new Employee("mak", "seller", "mak@mail.ru", 23634, 10000, 41);
+        employees[4] = new Employee("bryan", "manager", "bryan@mail.ru", 98754, 17000, 26);
 
-        while (true) {
-            humanTurn();
-            printMap();
-            if (checkWinLines(DOT_X, DOTS_TO_WIN)) {
-                System.out.println("Поздравляем! Вы выиграли!");
-                break;
-            }
-            if (isFull()) {
-                System.out.println("Ничья");
-                break;
-            }
-
-            aiTurn();
-            printMap();
-            if (checkWinLines(DOT_O, DOTS_TO_WIN)) {
-                System.out.println("Копьютер победил.");
-                break;
-            }
-            if (isFull()) {
-                System.out.println("Ничья");
-                break;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getAge() > 40) {
+                employees[i].info();
             }
         }
-    }
-
-    public static void initMap() {
-        map = new char[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                map[i][j] = DOT_EMPTY;
-            }
-        }
-    }
-
-    public static void printMap() {
-        System.out.print("  ");
-        for (int i = 1; i <= SIZE; i++) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < SIZE; i++) {
-            System.out.print(i + 1 + " ");
-            for (int j = 0; j < SIZE; j++) {
-                System.out.printf("%c ", map[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-    public static void humanTurn() {
-        int x, y;
-        do {
-            System.out.println("input X,Y");
-            x = sc.nextInt() - 1;
-            y = sc.nextInt() - 1;
-        } while (!isCellValid(y, x));
-        map[y][x] = DOT_X;
-    }
-
-    public static boolean isCellValid(int y, int x) {
-        if (y < 0 || x < 0 || y >= SIZE || x >= SIZE) {
-            return false;
-        }
-        return map[y][x] == DOT_EMPTY;
-    }
-
-    public static void aiTurn() {
-        int x, y;
-        do {
-            y = random.nextInt(SIZE);
-            x = random.nextInt(SIZE);
-        } while (!isCellValid(y, x));
-        map[y][x] = DOT_O;
-    }
-
-    public static boolean isFull() {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (map[i][j] == DOT_EMPTY) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    static boolean checkLine(int cy, int cx, int vy, int vx, char dot, int dotsToWin) {
-        if (cx + vx + (dotsToWin - 1) > SIZE - 1 || cy + vy * (dotsToWin - 1) > SIZE - 1 || cy + vy * (dotsToWin - 1) < 0) {
-            return false;
-        }
-
-        for (int i = 0; i < dotsToWin; i++) {
-            if (map[cy + i * vy][cx + i * vx] != dot) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean checkWinLines(char dot, int dotsToWin) {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (checkLine(i, j, 0, 1, dot, dotsToWin) ||
-                        checkLine(i, j, 1, 0, dot, dotsToWin) ||
-                        checkLine(i, j, 1, 1, dot, dotsToWin) ||
-                        checkLine(i, j, -1, 1, dot, dotsToWin)) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 }
 
