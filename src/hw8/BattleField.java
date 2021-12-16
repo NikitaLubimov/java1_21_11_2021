fc1b8cafb342a60a63f49b3ca4b5d12b51a90803
NikitaLubimov <ikarus11122@gmail.com>
2021-12-16 08:10:13 +0300

    hw8

---
+++ src/hw8/BattleField.java
@@ -0,0 +1,113 @@
+package hw8;
+
+import sun.rmi.runtime.Log;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+
+
+public class BattleField extends JPanel {
+    private GameWindow gameWindow;
+    private int size;
+    private int winningLength;
+
+
+    private boolean isInit;
+
+    private int cellWidth;
+    private int cellHeight;
+
+
+    public BattleField(GameWindow gameWindow){
+        this.gameWindow = gameWindow;
+        setBackground(Color.ORANGE);
+
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                int cellX = e.getX() / cellWidth;
+                int cellY = e.getY() / cellHeight;
+
+                if (!Logic.gameFinished) {
+                    Logic.humanTurn(cellX, cellY);
+                }
+            }
+        });
+    }
+
+
+    void startNewGame(int size, int winningLength) {
+        this.size = size;
+        this.winningLength = winningLength;
+
+        isInit = true;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (!isInit) {
+            return;
+        }
+
+        cellWidth = getWidth() / size;
+        cellHeight = getHeight() / size;
+
+        g.setColor(Color.DARK_GRAY);
+        ((Graphics2D) g).setStroke(new BasicStroke(2f));
+
+
+        for (int i = 1; i < size; i++) {
+            int y = i * cellHeight;
+            g.drawLine(0, y, getWidth(), y);
+        }
+
+        for (int i = 1; i < size; i++) {
+            int x = i * cellWidth;
+            g.drawLine(x, 0, x, getHeight());
+        }
+
+// определяем позицию Х в массиве для рисования
+        for (int i = 0; i < Logic.SIZE; i++) {
+            for (int j = 0; j < Logic.SIZE; j++) {
+                if (Logic.map[j][i] == Logic.DOT_X) {
+                    drawX(g, i, j);
+                }
+            }
+        }
+        repaint();
+// определяем позицию О в массиве для рисования
+        for (int i = 0; i < Logic.SIZE; i++) {
+            for (int j = 0; j < Logic.SIZE; j++) {
+                if (Logic.map[j][i] == Logic.DOT_O) {
+                    drawO(g,i,j);
+                }
+            }
+        }
+    }
+// Рисуем Х
+    private void drawX(Graphics g, int cellX, int cellY) {
+        g.setColor(Color.BLUE);
+        ((Graphics2D) g).setStroke(new BasicStroke(3f));
+
+        g.drawLine(cellX * cellWidth, cellY * cellHeight,
+                cellX * cellWidth + cellWidth, cellY * cellHeight + cellHeight);
+
+        g.drawLine(cellX * cellWidth + cellWidth, cellY * cellHeight,
+                cellX * cellWidth, cellY * cellHeight + cellHeight);
+    }
+// Рисуем О
+    private void drawO(Graphics g, int cellX, int cellY) {
+        g.setColor(Color.RED);
+        ((Graphics2D) g).setStroke(new BasicStroke(3f));
+
+        g.drawOval(cellX * cellWidth, cellY * cellHeight, cellWidth, cellHeight);
+    }
+}
